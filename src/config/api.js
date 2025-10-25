0b0737873976c31a6756811abdfb848c3f023885
Webcrafticx <webcraftix3@gmail.com>
2025-10-25 20:25:13 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:5000/api';
-// const API_BASE_URL = 'https://chatbot-backend-39p6.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://chatbot-backend-39p6.onrender.com/api';
 
 
 export default API_BASE_URL;
